b4547e3d94a5c5ce3a61905f9c2478461fa16cc0
ValeryNunez01 <72088086+ValeryNunez01@users.noreply.github.com>
2021-10-05 12:25:45 -0500

    Add files via upload

---
+++ CIFRADO_DES/src/cifrado_des/CIFRADO_DES.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cifrado_des;
+
+/**
+ *
+ * @author Valery
+ */
+public class CIFRADO_DES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventanauno vent = new Ventanauno();
+        vent.setVisible(true);
+        vent.setLocationRelativeTo(null);
+    }
+    
+}
